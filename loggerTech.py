0574d4c7025ea9c78b992e339cc8037ce6555118
Roodewald <pavel999977@gmail.com>
2023-10-13 19:35:34 +0500

    Get Post out

--- loggerTech.py
+++ loggerTech.py
@@ -20,7 +20,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
+logging.getLogger("httpx").setLevel(logging.WARNING)
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
